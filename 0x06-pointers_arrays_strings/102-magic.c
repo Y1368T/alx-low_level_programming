2c18bd56fb125a6398651aa47b5d8c222cbda769
Yeabsira Tsegaye <yeabsiratsegaye0@gmail.com>
2022-12-22 15:31:32 -0800

    jhg

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints exactly your line of code here...
+ * main - prints exactly ine of code here...
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
@@ -13,14 +14,18 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	return (0);
 /*
  * write your line of code here...
  * Remember:
- * - you are not allowed to use a
- *   - you are not allowed to modify p
- *   - only one statement
- *   - you are not allowed to code anything else than this line of code
+ * - you are not allowed to use a in your new line of code
+ * - you are not allowed to modify p
+ * - you can only write one statement
+ * - you are not allowed to use ,
+ * - you are not allowed to code anything else than the line of expected line
+ * - your code should be written at line 19, before the ;
+ * - do not remove anything from the initial code
+ * - and don’t change anything but the line of code you are adding
+ * - you are allowed to use the standard library
  */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
